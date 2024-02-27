c48e36d08cca3942eac26a0c551f542eba7e74b8
Bruno Torres <bruno_augusto_torres@yahoo.com.br>
2024-02-27 19:14:24 -0300

    Alteração do nome do arquivo principal

--- horario/src/main/java/br/com/horario/HorarioApplication.java
+++ horario/src/main/java/br/com/horario/HorarioApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Horario20241Application {
+public class HorarioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Horario20241Application.class, args);
+		SpringApplication.run(HorarioApplication.class, args);
 	}
 
 }
--- horario/src/test/java/br/com/horario/HorarioApplicationTests.java
+++ horario/src/test/java/br/com/horario/HorarioApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Horario20241ApplicationTests {
+class HorarioApplicationTests {
 
 	@Test
 	void contextLoads() {
